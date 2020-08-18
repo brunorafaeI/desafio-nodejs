7f387a4b323c619db40f299c13650ad87157f348
Bruno Rafael <brunorafaelap@gmail.com>
2020-08-18 21:14:01 +0200

    Added create a new repository with likes value default '0' from the router POST /repositories

--- src/app.js
+++ src/app.js
@@ -15,7 +15,13 @@ app.get("/repositories", (request, response) => {
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  const { title, url, techs } = request.body;
+
+  const repository = { id: uuid(), title, url, techs, likes: 0 };
+
+  repositories.push(repository);
+
+  return response.status(201).json(repository);
 });
 
 app.put("/repositories/:id", (request, response) => {
